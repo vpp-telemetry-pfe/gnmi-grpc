70809f3cddf9a32f78286628a3dac885d4ab3aba
YohanPipereau <yohan.pipereau@gmail.com>
2019-01-04 00:57:24 +0100

    correct Docker environment

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,3 @@ FROM yohanpipereau/grpcpp:1.12.0
 COPY . /home/app/
 WORKDIR /home/app/
 ENTRYPOINT ["./entrypoint.sh"]
-CMD ["./build/gnmi_server"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,7 @@ apt-get update && apt-get install -y vpp vpp-dev
 
 make
 
+#On first argument, cut out 1st char starting at 0 and check it's a '-'
 if [ "${1:0:1}" = '-' ]; then
     set -- ./build/gnmi_server "$@"
 fi
